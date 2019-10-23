1da6f758aa7a2212674a695d3d0552d521f3fd7b
Cristina Moreno Medran <moreno.medran@gmail.com>
2019-10-23 15:34:22 +0200

    objects colliding up to this point

--- tetris.js
+++ tetris.js
@@ -12,10 +12,33 @@ const matrix = [
     [0, 1, 0]
 ]
 
+
+function collide (arena, player){
+    const [m, o] = [player.matrix, player.pos]; 
+    for(let y=0; y < m.length; ++y ){
+        for(let x=0; x < m[y].length; ++x){
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0 ){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function createMatrix(w, h){
+    const matrix = [];
+    while(h--){
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw(){
     context.fillStyle = "#000";
     // https://developer.mozilla.org/es/docs/Web/API/CanvasRenderingContext2D/fillRect
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena, {x:0, y:0});
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -32,8 +55,24 @@ function drawMatrix(matrix, offset){
     });
 }
 
+
+function merge (arena, player){
+    player.matrix.forEach((row, y)  => {
+        row.forEach((value, x) => {
+            if (value !== 0){
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        })
+    }) 
+}
+
 function playerDrop(){
     player.pos.y++;
+    if(collide(arena, player)){
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    };
     dropCounter = 0;
 }
 
@@ -52,6 +91,9 @@ function update (time = 0){
     // https://developer.mozilla.org/es/docs/Web/API/Window/requestAnimationFrame
     requestAnimationFrame(update);
 }
+
+
+const arena = createMatrix(12, 20); 
 
 
 const player = {
