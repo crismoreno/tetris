956648ab2d02f2d6bd1f84f31d2d07efbc60e03e
Cristina Moreno Medran <moreno.medran@gmail.com>
2019-10-24 10:41:25 +0200

    game is finished in this commit

--- tetris.js
+++ tetris.js
@@ -7,6 +7,23 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
+function arenaSweep(){
+    let rowCount = 1; 
+    outer: for (let y = arena.length -1; y > 0; --y ){
+        for (let x = 0; x < arena[y].length; ++x){
+            if(arena[y][x] === 0){
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
+
+
 function collide (arena, player){
     const [m, o] = [player.matrix, player.pos]; 
     for(let y=0; y < m.length; ++y ){
@@ -38,38 +55,38 @@ function createPiece(type){
         ]
     }else if(type === 'O'){
         return[
-            [1, 1],
-            [1, 1]
+            [2, 2],
+            [2, 2]
         ]
     }else if (type === 'L'){
         return[
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1]
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3]
         ]
     }else if (type === 'J'){
         return[
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0]
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0]
         ]
     }else if (type === 'I'){
         return[
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0]
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0]
         ]
     }else if (type === 'S'){
         return[
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0]
         ]
     }else if (type === 'Z'){
         return[
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0]
         ]
     }
@@ -92,7 +109,7 @@ function drawMatrix(matrix, offset){
     matrix.forEach((row, y) => {
         row.forEach((value, x) =>{
             if (value !== 0){
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -116,7 +133,9 @@ function playerDrop(){
         player.pos.y--;
         merge(arena, player);
         playerReset();
-        player.pos.y = 0;
+        // player.pos.y = 0;
+        arenaSweep();
+        updateScore();
     };
     dropCounter = 0;
 }
@@ -137,6 +156,8 @@ function playerReset(){
 
     if(collide(arena, player)){
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -190,13 +211,28 @@ function update (time = 0){
     requestAnimationFrame(update);
 }
 
+function updateScore(){
+    document.getElementById('score').innerText = player.score;
+}
 
+
+const colors = [
+    null, 
+    'red', 
+    'blue', 
+    'violet', 
+    'purple',
+    'orange', 
+    'teal', 
+    'pink'
+]
 const arena = createMatrix(12, 20); 
 
 
 const player = {
-    pos: {x:5, y:5},
-    matrix: createPiece('T'),
+    pos: {x:0, y:0},
+    matrix: null,
+    score: 0
 }
 
 document.addEventListener('keydown', event =>{
@@ -217,4 +253,6 @@ document.addEventListener('keydown', event =>{
 });
 
 
+playerReset();
+updateScore();
 update();
