a5acc709902cdc4969d26a0c25245ded12b0ec21
JoseMoriglioni <56884719+JoseMoriglioni@users.noreply.github.com>
2019-10-24 11:46:16 +0200

    Update tetris.js
    Changed cretePiece function into switch case

--- tetris.js
+++ tetris.js
@@ -47,48 +47,64 @@ function createMatrix(w, h){
 
 
 function createPiece(type){
-    if(type === 'T'){
+    
+    switch (type){
+    case 'T':
         return[
             [0, 0, 0],
             [1, 1, 1],
             [0, 1, 0]
         ]
-    }else if(type === 'O'){
+    break;
+
+    case 'O':
         return[
             [2, 2],
             [2, 2]
         ]
-    }else if (type === 'L'){
+    break;
+
+    case 'L':
         return[
             [0, 3, 0],
             [0, 3, 0],
             [0, 3, 3]
-        ]
-    }else if (type === 'J'){
+        ]    
+    break;
+
+    case 'J':
         return[
             [0, 4, 0],
             [0, 4, 0],
             [4, 4, 0]
-        ]
-    }else if (type === 'I'){
+        ]    
+    break;
+
+    case 'I':
         return[
             [0, 5, 0, 0],
             [0, 5, 0, 0],
             [0, 5, 0, 0],
             [0, 5, 0, 0]
-        ]
-    }else if (type === 'S'){
+        ]    
+    break;
+
+    case 'S':
         return[
             [0, 6, 6],
             [6, 6, 0],
             [0, 0, 0]
-        ]
-    }else if (type === 'Z'){
+        ]    
+    break;
+
+    case 'Z':
         return[
             [7, 7, 0],
             [0, 7, 7],
             [0, 0, 0]
-        ]
+        ]    
+    break;
+
     }
 
 }
